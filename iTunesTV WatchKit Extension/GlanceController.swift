e891b2328dcadd864c867b6e555f555080d3b40f
Mr Drew Westcott <drew.westcott@me.com>
2015-12-31 16:10:02 +0000

    Initial Commit

---
+++ iTunesTV WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  iTunesTV WatchKit Extension
+//
+//  Created by Drew Westcott on 31/12/2015.
+//  Copyright © 2015 Drew Westcott. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
